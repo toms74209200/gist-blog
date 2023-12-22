52c5d6ddca56bf07075d56ee25cbe1c513a874d1
toms74209200 <motomotomato@gmail.com>
2023-12-22 14:58:14 +0000

    :art: Change markdown rendering library

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /// <reference lib="dom"/>
 import { contents } from "./contents.ts";
 import { requestGists } from "./requestGists.ts";
-import { marked } from "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
+import { render } from "https://deno.land/x/gfm/mod.ts";
 const main = async () => {
   const gists = await requestGists(contents);
 
@@ -10,7 +10,7 @@ const main = async () => {
     Object.keys(files).forEach((fileName) => {
       const fileContent = files[fileName].content;
       const contentElement = document.getElementById("content");
-      contentElement!.innerHTML += marked.parse(fileContent);
+      contentElement!.innerHTML += render(fileContent);
     });
   });
 };
