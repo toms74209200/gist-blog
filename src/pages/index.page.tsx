faaa8d0d7ac408e4dbeba5035d469fe84e4b8eff
toms74209200 <motomotomato@gmail.com>
2023-12-25 14:19:20 +0900

    :art: Change layout

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -8,7 +8,7 @@ export const layout = "layout.tsx";
 const BlogIndexPage = async () => {
   const posts = await getPosts(contents);
   return (
-    <main class="max-w-screen-md px-4 pt-16 mx-auto">
+    <main>
       <h1 class="text-5xl font-bold">Blog</h1>
       <div class="mt-8">
         {posts.map((post) => <PostCard post={post} />)}
