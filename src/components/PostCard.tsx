f913b5d08196990a2c088c0f3ece1b532adb193d
toms74209200 <motomotomato@gmail.com>
2023-12-25 21:43:25 +0900

    :bug: Fix relative URL

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -4,7 +4,7 @@ export const PostCard = (props: { post: Post }) => {
   const { post } = props;
   return (
     <div class="py-8 border(t gray-200)">
-      <a class="sm:col-span-2" href={`/${post.slug}`}>
+      <a class="sm:col-span-2" href={`./${post.slug}`}>
         <h3 class="text(3xl gray-900) font-bold">
           {post.title}
         </h3>
