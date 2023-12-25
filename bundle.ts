d4c49c94f24e6ad809b6aad8d978df6a934c11fe
toms74209200 <motomotomato@gmail.com>
2023-12-25 17:32:56 +0900

    :fire: Remove unused files

--- bundle.ts
+++
@@ -1,9 +0,0 @@
-import { bundle } from "emit";
-
-const { code } = await bundle(
-  new URL(import.meta.resolve("./src/main.ts")),
-  {
-    importMap: "deno.json",
-  },
-);
-Deno.writeTextFile("./public/main.js", code);
