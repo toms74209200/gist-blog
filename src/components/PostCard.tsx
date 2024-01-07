1c62587a9b6996297e3717b03ee761fc6f9b3ea5
toms74209200 <motomotomato@gmail.com>
2024-01-07 23:05:37 +0900

    :lipstick: Update style of PostCard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -3,9 +3,9 @@ import { Post } from "@/src/models/posts.ts";
 export const PostCard = (props: { post: Post }) => {
   const { post } = props;
   return (
-    <div class="py-8 border(t gray-200)">
-      <a class="sm:col-span-2" href={`./${post.slug}`}>
-        <h3 class="text(3xl gray-900) font-bold">
+    <div class="my-4 py-4 border-t border-gray-200 hover:bg-white hover:opacity-75">
+      <a class="block sm:col-span-2" href={`./${post.slug}`}>
+        <h3 class="text-3xl text-gray-900 font-bold">
           {post.title}
         </h3>
         <time class="text-gray-500">
