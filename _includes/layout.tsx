cb7bc35233174efa2e24cc065b31d77a8985f45c
toms74209200 <motomotomato@gmail.com>
2024-03-27 14:24:03 +0900

    :sparkles: Update layout.tsx

--- _includes/layout.tsx
+++ _includes/layout.tsx
@@ -1,6 +1,8 @@
 export default ({ title, children }: Lume.Data, helpers: Lume.Helpers) => (
   <html>
     <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
       <title>{title}</title>
       <link rel="stylesheet" href="static/styles.css"></link>
     </head>
