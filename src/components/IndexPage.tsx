fd0ddb5dd411b0c618f5e2b00a2ae596f151e641
toms74209200 <motomotomato@gmail.com>
2024-11-09 13:34:33 +0900

    :sparkles: Add index page

---
+++ src/components/IndexPage.tsx
@@ -0,0 +1,19 @@
+import type { BlogConfig } from "../models/fetchConfig";
+import type { Post } from "../models/posts";
+import { PostCard } from "./PostCard";
+
+interface IndexPageProps {
+  config: BlogConfig;
+  posts: Post[];
+}
+
+export const IndexPage = ({ config, posts }: IndexPageProps) => {
+  <main className={"pt-16"}>
+    <h1 className="text-5xl font-bold">{config.title}</h1>
+    <div className="mt-8">
+      {posts.map((post) => (
+        <PostCard post={post} />
+      ))}
+    </div>
+  </main>;
+};
