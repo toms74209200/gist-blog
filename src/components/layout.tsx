7332b81c8a4e40f82df8c943b7055ed7b3b35fca
toms74209200 <motomotomato@gmail.com>
2025-02-07 21:44:36 +0900

    :arrow_up: Upgrade Tailwind CSS to v4

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,11 @@ import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <body className={["max-w-screen-md", "px-4", "py-8", "mx-auto"].join(" ")}>
+    <body
+      className={["max-w-(--breakpoint-md)", "px-4", "py-8", "mx-auto"].join(
+        " "
+      )}
+    >
       {children}
     </body>
   );
