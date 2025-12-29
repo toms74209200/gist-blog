92b026b3996c0203c9b32dfeee3bc0a3e481e81e
toms74209200 <motomotomato@gmail.com>
2025-12-29 23:25:47 +0900

    :white_check_mark: Update visual test for target path

--- visual_test/tests/index.spec.ts
+++ visual_test/tests/index.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("has h1", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
+  await page.goto("http://localhost:8000/gist-blog/");
 
   // Expect a title "to contain" a substring.
   const h1 = page.locator("h1").filter({
